95c7cc67250cce4923f553a5217c4d57e0d7cb7e
frodoman <my_adress_2003@hotmail.com>
2019-03-24 19:00:27 +0000

    Create work space and projects for models and App.

---
+++ Posts/Posts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Posts
+//
+//  Created by Xinghou Liu on 24/03/2019.
+//  Copyright © 2019 Xmart Soft Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
