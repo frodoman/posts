37c2d1c737a8b382edf7c606c67e999289f7e754
frodoman <my_adress_2003@hotmail.com>
2019-03-24 19:04:19 +0000

    Correct project settings.

---
+++ Models/Models/Models.h
@@ -0,0 +1,19 @@
+//
+//  Models.h
+//  Models
+//
+//  Created by Xinghou Liu on 24/03/2019.
+//  Copyright © 2019 Xmart Soft Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Models.
+FOUNDATION_EXPORT double ModelsVersionNumber;
+
+//! Project version string for Models.
+FOUNDATION_EXPORT const unsigned char ModelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Models/PublicHeader.h>
+
+
